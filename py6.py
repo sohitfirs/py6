bac1e62d8dda4ff3c28e77eb991e6bdab7fa14d9
sohitfirs <126562924+sohitfirs@users.noreply.github.com>
2023-11-07 12:23:20 +0300

    Add files via upload

---
+++ py6.py
@@ -0,0 +1,52 @@
+﻿# Задание 1
+
+n = int(input('Кол-во строк: '))
+l = []
+
+for i in range(n):
+    l.append(int(input(f'Введите число {i+1}: ')))
+
+l.reverse()
+
+print(*l) 
+
+# Задание 2
+
+n = int(input('Кол-во строк: '))
+l = []
+
+for i in range(n):
+    l.append(int(input(f'Введите число {i+1}: ')))
+
+l.insert(0, l.pop())
+
+print(*l)
+
+# Задание 3
+
+m = int(input('Максимальная масса: '))
+n = int(input('Кол-во рыбаков: '))
+l = []
+count = 0
+
+for i in range(n):
+    l.append(int(input(f'Введите вес рыбака {i}: ')))
+
+x = len(l)
+
+for i in range(x):        
+    if l != [] and len(l) != 1 and min(l) + max(l) <= m:
+        count += 1
+        l.pop((l.index(max(l))))
+        l.pop((l.index(min(l))))
+    elif l != []: 
+        l.pop((l.index(max(l))))
+        count += 1
+    else:
+        continue
+
+print(count)
+input()
+
+
+  
